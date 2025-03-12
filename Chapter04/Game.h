15143ebc10b4282820ccd8ccf4881132f87e2183
maedakeiji0221 <58039012+maedakeiji0221@users.noreply.github.com>
2025-03-12 21:50:18 +0900

    Cursor使用時によるプチ変更

--- Chapter04/Game.h
+++ Chapter04/Game.h
@@ -1,9 +1,9 @@
 // ----------------------------------------------------------------
-// From Game Programming in C++ by Sanjay Madhav
+// 「C++によるゲームプログラミング」by Sanjay Madhav より
 // Copyright (C) 2017 Sanjay Madhav. All rights reserved.
-// 
-// Released under the BSD License
-// See LICENSE in root directory for full details.
+//
+// BSDライセンスのもとでリリース
+// 詳細はルートディレクトリのLICENSEを参照
 // ----------------------------------------------------------------
 
 #pragma once
@@ -21,44 +21,45 @@ public:
 	void RunLoop();
 	void Shutdown();
 
-	void AddActor(class Actor* actor);
-	void RemoveActor(class Actor* actor);
+	void AddActor(class Actor *actor);
+	void RemoveActor(class Actor *actor);
 
-	void AddSprite(class SpriteComponent* sprite);
-	void RemoveSprite(class SpriteComponent* sprite);
-	
-	SDL_Texture* GetTexture(const std::string& fileName);
-	
-	class Grid* GetGrid() { return mGrid; }
-	std::vector<class Enemy*>& GetEnemies() { return mEnemies; }
-	class Enemy* GetNearestEnemy(const Vector2& pos);
+	void AddSprite(class SpriteComponent *sprite);
+	void RemoveSprite(class SpriteComponent *sprite);
+
+	SDL_Texture *GetTexture(const std::string &fileName);
+
+	class Grid *GetGrid() { return mGrid; }
+	std::vector<class Enemy *> &GetEnemies() { return mEnemies; }
+	class Enemy *GetNearestEnemy(const Vector2 &pos);
+
 private:
 	void ProcessInput();
 	void UpdateGame();
 	void GenerateOutput();
 	void LoadData();
 	void UnloadData();
-	
-	// Map of textures loaded
-	std::unordered_map<std::string, SDL_Texture*> mTextures;
 
-	// All the actors in the game
-	std::vector<class Actor*> mActors;
-	// Any pending actors
-	std::vector<class Actor*> mPendingActors;
+	// ロードされたテクスチャのマップ
+	std::unordered_map<std::string, SDL_Texture *> mTextures;
 
-	// All the sprite components drawn
-	std::vector<class SpriteComponent*> mSprites;
+	// ゲーム内のすべてのアクター
+	std::vector<class Actor *> mActors;
+	// 保留中のアクター
+	std::vector<class Actor *> mPendingActors;
 
-	SDL_Window* mWindow;
-	SDL_Renderer* mRenderer;
+	// 描画されるすべてのスプライトコンポーネント
+	std::vector<class SpriteComponent *> mSprites;
+
+	SDL_Window *mWindow;
+	SDL_Renderer *mRenderer;
 	Uint32 mTicksCount;
 	bool mIsRunning;
-	// Track if we're updating actors right now
+	// 現在アクターを更新中かどうかを追跡
 	bool mUpdatingActors;
 
-	// Game-specific
-	std::vector<class Enemy*> mEnemies;
-	class Grid* mGrid;
+	// ゲーム固有
+	std::vector<class Enemy *> mEnemies;
+	class Grid *mGrid;
 	float mNextEnemy;
 };
