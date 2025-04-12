0a42d5879140a941e822e244911401cb81b4809b
maedakeiji0221 <58039012+maedakeiji0221@users.noreply.github.com>
2025-04-13 00:45:36 +0900

    フォーマッタとC++設定ファイルの変更

--- Chapter04/Grid.h
+++ Chapter04/Grid.h
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------
 // From Game Programming in C++ by Sanjay Madhav
 // Copyright (C) 2017 Sanjay Madhav. All rights reserved.
-// 
+//
 // Released under the BSD License
 // See LICENSE in root directory for full details.
 // ----------------------------------------------------------------
@@ -13,38 +13,39 @@
 class Grid : public Actor
 {
 public:
-	Grid(class Game* game);
-	
+	Grid(class Game *game);
+
 	// Handle a mouse click at the x/y screen locations
 	void ProcessClick(int x, int y);
-	
+
 	// Use A* to find a path
-	bool FindPath(class Tile* start, class Tile* goal);
-	
+	bool FindPath(class Tile *start, class Tile *goal);
+
 	// Try to build a tower
 	void BuildTower();
-	
+
 	// Get start/end tile
-	class Tile* GetStartTile();
-	class Tile* GetEndTile();
+	class Tile *GetStartTile();
+	class Tile *GetEndTile();
 
 	void UpdateActor(float deltaTime) override;
+
 private:
 	// Select a specific tile
 	void SelectTile(size_t row, size_t col);
-	
+
 	// Update textures for tiles on path
-	void UpdatePathTiles(class Tile* start);
-	
+	void UpdatePathTiles(class Tile *start);
+
 	// Currently selected tile
-	class Tile* mSelectedTile;
-	
+	class Tile *mSelectedTile;
+
 	// 2D vector of tiles in grid
-	std::vector<std::vector<class Tile*>> mTiles;
-	
+	std::vector<std::vector<class Tile *>> mTiles;
+
 	// Time until next enemy
 	float mNextEnemy;
-	
+
 	// Rows/columns in grid
 	const size_t NumRows = 7;
 	const size_t NumCols = 16;
